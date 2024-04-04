9131cd6fc0d03510c4a08d0b1de82cab448ca7a6
shumail pasha <Shumail.Pasha@tpv-tech.com>
2024-04-04 17:11:18 +0530

    Pasha has changed this

--- scenarios/Schedules.scenario.ts
+++ scenarios/Schedules.scenario.ts
@@ -6,16 +6,11 @@ import UpdatePowerSchedule from './tests/UpdatePowerSchedule';
 // import { client, poll } from '../apiClient/client';
 // import { Log } from '../utils/Log';
 
-// jest.setTimeout(90_000);
+jest.setTimeout(90_000);
 
-// const fs = require('fs');
-// const path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-// const fs = require('fs');
-// const path = require('path');
-
-// const fs = require('fs');
-// const path = require('path');
 /*
 global is a global object in Node.js, similar to window in a browser environment.
 __DISPLAYS__ is a variable or property attached to the global object. It seems to hold an array of display data.
